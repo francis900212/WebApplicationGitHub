5f285697e9dc13d27f81e0d6a280da6178a75bec
Francisco Fernandez <ffernandezcasas@gmail.com>
2018-05-26 15:34:34 -0400

    first time

---
+++ WebApplicationGitHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationGitHub.Startup))]
+namespace WebApplicationGitHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
